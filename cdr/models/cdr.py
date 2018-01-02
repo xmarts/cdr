9896d5da41998e8fe71b93bd1af536f03cbb8ea4
viernes <luis.muro@xmarts.com>
2018-01-02 12:06:26 -0600

    Cambio de tipo para inicio y fin de time a string Agregadas dos catalogos en tipo destino

--- cdr/models/cdr.py
+++ cdr/models/cdr.py
@@ -14,8 +14,8 @@ class Cdr(models.Model):
     numerob= fields.Char(string="Número B",help="Número B con 10 digitos")
     poblacion= fields.Char(string="Poblacion Destino",help="Población Destino")
     fecha= fields.Date(string="Fecha Llamada",help="Fecha de realización de llamada")
-    inicio=fields.Datetime(string="Inicio",help="Hora de inicio de llamda")
-    fin=fields.Datetime(string="Fin",help="Hora de termino de la llamada")
+    inicio=fields.Char(string="Inicio",help="Hora de inicio de llamda")
+    fin=fields.Char(string="Fin",help="Hora de termino de la llamada")
     min=fields.Integer(string="Min",help="Duracción de la llamada en minutos")
     montobase=fields.Float(string="Monto Base",help="Precio base")
     montofinal=fields.Float(string="Monto Final",help="Precio base")
@@ -25,7 +25,7 @@ class Cdr(models.Model):
     tipoorigen=fields.Char(string="Tipo Tel Origen",help="Tipo de Telefono Origen")
     tipodestino=fields.Selection([('800 INTERNACIONAL','800 INTERNACIONAL'),('800 NACIONAL','800 NACIONAL'),('LD INTERNACIONAL','LD INTERNACIONAL'),
                                    ('LD INTERNACIONAL CELULAR','LD INTERNACIONAL CELULAR'),
-                                   ('LD NACIONAL','LD NACIONAL'),('LD NACIONAL CELULAR','LD NACIONAL CELULAR')],
+                                   ('LD NACIONAL','LD NACIONAL'),('LD NACIONAL CELULAR','LD NACIONAL CELULAR'),('LOCAL','LOCAL'),('LOCAL CELULAR','LOCAL CELULAR')],
                                 string="Tipo Tel Destino",help="Tipo de  Telefono Destino")
     producto=fields.Char(string="Producto",help="Producto Ofertado")
     plan=fields.Char(string="Plan",help="Plan Ofertado")
